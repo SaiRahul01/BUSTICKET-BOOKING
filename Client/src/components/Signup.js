efe918bc48c74011a2742cc10755d8c2947163d8
SaiRahul01 <84556711+SaiRahul01@users.noreply.github.com>
2022-04-17 21:20:47 +0530

    Update Signup.js

--- Client/src/components/Signup.js
+++ Client/src/components/Signup.js
@@ -1,4 +1,4 @@
-import  Axios  from 'axios'
+import Axios from 'axios'
 import React, { useState } from 'react'
 // eslint-disable-next-line no-unused-vars
 import { ToastContainer, toast } from 'react-toastify';
@@ -9,38 +9,35 @@ import './main.css'
 
 export default function Signup() {
 
-        const [name, setname] = useState('')
-        const [email, setemail] = useState('')
-        const [password, setpassword] = useState('')
-        const [mobilenumber, setmobilenumber] = useState('')
-        const [age, setage] = useState(0)
-        const [city, setcity] = useState('')
-        const [pincode, setpincode] = useState(0)
-        const [streetname, setstreetname] = useState('')
-        const [houseno, sethouseno] = useState(0)
-        const [gender, setgender] = useState('')
+    const [name, setname] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [mobilenumber, setmobilenumber] = useState('')
+    const [age, setage] = useState(0)
+    const [city, setcity] = useState('')
+    const [pincode, setpincode] = useState(0)
+    const [streetname, setstreetname] = useState('')
+    const [houseno, sethouseno] = useState(0)
+    const [gender, setgender] = useState('')
 
-        const [regstatus, setregstatus] = useState('')
+    const [regstatus, setregstatus] = useState('')
 
 
-    const togglepwdvis=(e)=>{
-        let x=document.getElementById('pwdfield');
-        if(x.type==='password')
-        {
-            x.type='text';
+    const togglepwdvis = (e) => {
+        let x = document.getElementById('pwdfield');
+        if (x.type === 'password') {
+            x.type = 'text';
         }
-        else
-        {
-            x.type='password';
+        else {
+            x.type = 'password';
         }
     }
 
 
 
-    const handleregister=(e)=>{
-        if(name==='' || email==='' || password==='' || mobilenumber==='' ||age===''||city===''|| pincode===''|| streetname===''||houseno==='' )
-        {
-           toast.error('Enter all fields')
+    const handleregister = (e) => {
+        if (name === '' || email === '' || password === '' || mobilenumber === '' || age === '' || city === '' || pincode === '' || streetname === '' || houseno === '') {
+            toast.error('Enter all fields')
             return;
         }
 
@@ -48,8 +45,7 @@ export default function Signup() {
         if (emailsArray != null && emailsArray.length) {
 
         }
-        else
-        {
+        else {
             toast.success('🦄 Wow so easy!', {
                 position: "top-right",
                 autoClose: 3000,
@@ -58,16 +54,14 @@ export default function Signup() {
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                });
+            });
             return;
         }
         e.preventDefault();
-        if((/^\d{10}$/).test(mobilenumber) || (mobilenumber.value<=9999999999 && mobilenumber.value>=1000000000))
-        {
+        if ((/^\d{10}$/).test(mobilenumber) || (mobilenumber.value <= 9999999999 && mobilenumber.value >= 1000000000)) {
 
         }
-        else
-        {
+        else {
             toast('Mobile Number is not valid');
 
             return;
@@ -75,26 +69,24 @@ export default function Signup() {
 
 
 
-        Axios.post('http://localhost:3001/registeruser',{
-            name:name,
-            email:email,
-            password:password,
-            mobilenumber:mobilenumber,
-            age:age,
-            city:city,
-            pincode:pincode,
-            houseno:houseno,
-            streetname:streetname,
-            gender:gender
+        Axios.post('http://localhost:3001/registeruser', {
+            name: name,
+            email: email,
+            password: password,
+            mobilenumber: mobilenumber,
+            age: age,
+            city: city,
+            pincode: pincode,
+            houseno: houseno,
+            streetname: streetname,
+            gender: gender
 
         }).then(
-            (response)=>{
-                if(response.data.op)
-                {
+            (response) => {
+                if (response.data.op) {
                     toast("User Already Exists !! ")
                 }
-                else
-                {
+                else {
                     toast("Registered Successfully!")
                 }
                 // console.log(response.data);
@@ -123,10 +115,10 @@ export default function Signup() {
 
     }
 
-  return (
+    return (
 
-    <>
-    {/* <ToastContainer
+        <>
+            {/* <ToastContainer
         position="top-right"
         autoClose={1000}
         hideProgressBar={false}
@@ -139,97 +131,140 @@ export default function Signup() {
         closeButton={false}
         /> */}
 
-    
-         <div className="page-wrapper bg-gra-01 p-t-30 p-b-80 font-poppins"   >
-   
-             <div></div>
 
-        <div className="wrapper  wrapper--w960 "  style={{marginLeft:'240px'}} >
-                         <br/>
-                         <br/>
-                         <br/>
+            <div className="page-wrapper bg-gra-01 p-t-30 p-b-80 font-poppins"   >
+
+                <div></div>
+
+                <div className="wrapper  wrapper--w960 " style={{ marginLeft: '240px' }} >
+                    <br />
+                    <br />
+                    <br />
                     <h2 className="title text-center">Registration </h2>
 
                     <form method="POST"  >
-                       <div className="fc">
+                        <div className="fc">
 
-                        <div className="input-group">
-                            <input className="input--style-3" type="text" onChange={e=>setname(e.target.value)} placeholder="Name" name="username" required />
+                            <div className="input-group">
+                                <input className="input--style-3" type="text" onChange={e => setname(e.target.value)} placeholder="Name" name="username" required />
+                            </div>
+
+                            <div className="input-group">
+                                <input className="input--style-3" type="text" pattern="[1-9]{1}[0-9]{9}" onChange={e => setmobilenumber(e.target.value)} placeholder="Mobile Number" name="mobilenumber" required />
+                            </div>
                         </div>
 
-                        <div className="input-group">
-                            <input className="input--style-3" type="text" pattern="[1-9]{1}[0-9]{9}" onChange={e=>setmobilenumber(e.target.value)}  placeholder="Mobile Number" name="mobilenumber"  required />
-                        </div>
-                       </div>
+                        <div className="fc">
 
-                    <div className="fc">
+                            <div className="input-group">
+                                <input className="input--style-3" type="number" onChange={e => setage(e.target.value)} placeholder="Age" name="age" required />
 
-                         <div className="input-group">
-                             <input className="input--style-3" type="number" onChange={e=>setage(e.target.value)} placeholder="Age" name="age"  required />
+                            </div>
 
-                         </div>
-
-                          {/* <div>
+                            {/* <div>
                              <p style={{color:"white"}}>Gender</p>
                              <input type="radio"></input>
                                  <label>Male</label>
                          </div> */}
-                         <div className="input-group">
+                            {/* <div className="input-group">
                              <input className="input--style-3" type="text" onChange={e=>setgender(e.target.value)} placeholder="Gender" name="age"  required />
 
-                         </div>
+                         </div> */}
+                            <div class="col-md-6 mb-4">
 
-                    
-                     </div>
+                               
 
-               <div className="fc">
+                                <div class="form-check form-check-inline">
+                                <h4 style={{color:'white'}} class="mb-2 pb-1">Gender </h4>
+                                    <input
+                                        class="form-check-input"
+                                        type="radio"
+                                        name="inlineRadioOptions"
+                                        id="femaleGender"
+                                        value="option1"
+                                      
+                                        onChange={e=>setgender('Female')} 
+                                    />
+                                    <label style={{color:'white'}} class="form-check-label" for="femaleGender">Female</label>
+                                </div>
 
-                        <div className="input-group col">
-                            <input className="input--style-3" type="email" onChange={e=>setemail(e.target.value)} placeholder="Email" name="email"  required />
-                        </div>
+                                <div class="form-check form-check-inline">
+                                    <input
+                                        class="form-check-input"
+                                        type="radio"
+                                        name="inlineRadioOptions"
+                                        id="maleGender"
+                                        value="option2"
+                                        onChange={e=>setgender('Male')} 
+                                    />
+                                    <label style={{color:'white'}} class="form-check-label" for="maleGender">Male</label>
+                                </div>
 
-                        <div className="input-group" style={{display:'flex',alignItems:'center'}}>
-                            <input className="input--style-3" id='pwdfield' onChange={e=>setpassword(e.target.value)}  type="password" placeholder="Password" name="password"  required />
-
-                            <div onClick={togglepwdvis}>
-                            <span style={{color:'white',float: 'right',marginLeft: '-25px',marginTop: '0px',position: 'relative',zIndex:'  2'}}
-                             toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                <div class="form-check form-check-inline">
+                                    <input
+                                        class="form-check-input"
+                                        type="radio"
+                                        name="inlineRadioOptions"
+                                        id="otherGender"
+                                        value="option3"
+                                        onChange={e=>setgender('Other')} 
+                                    />
+                                    <label style={{color:'white'}} class="form-check-label" for="otherGender">Other</label>
+                                </div>
 
                             </div>
 
+
                         </div>
-                          </div>
+
+                        <div className="fc">
+
+                            <div className="input-group col">
+                                <input className="input--style-3" type="email" onChange={e => setemail(e.target.value)} placeholder="Email" name="email" required />
+                            </div>
+
+                            <div className="input-group" style={{ display: 'flex', alignItems: 'center' }}>
+                                <input className="input--style-3" id='pwdfield' onChange={e => setpassword(e.target.value)} type="password" placeholder="Password" name="password" required />
+
+                                <div onClick={togglepwdvis}>
+                                    <span style={{ color: 'white', float: 'right', marginLeft: '-25px', marginTop: '0px', position: 'relative', zIndex: '  2' }}
+                                        toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
+
+                                </div>
+
+                            </div>
+                        </div>
 
 
 
                         <br />
-                       <div className="fc">
+                        <div className="fc">
 
-                        <div className="input-group">
-                            <input className="input--style-3" type="text" onChange={e=>setcity(e.target.value)} placeholder="City" name="email"  required />
+                            <div className="input-group">
+                                <input className="input--style-3" type="text" onChange={e => setcity(e.target.value)} placeholder="City" name="email" required />
 
-                         </div>
+                            </div>
 
-                        <div className="input-group">
-                            <input className="input--style-3" type="number" onChange={e=>setpincode(e.target.value)} placeholder="Pincode" name="email"  required />
+                            <div className="input-group">
+                                <input className="input--style-3" type="number" onChange={e => setpincode(e.target.value)} placeholder="Pincode" name="email" required />
 
-                        </div>
-
-                        </div>
-
-                    <div className="fc">
-
-                        <div className="input-group">
-                            <input className="input--style-3" type="text" onChange={e=>sethouseno(e.target.value)} placeholder="House no" name="email"  required />
+                            </div>
 
                         </div>
 
-                        <div className="input-group">
-                            <input className="input--style-3" type="text" onChange={e=>setstreetname(e.target.value)} placeholder="Street Name" name="email"  required />
+                        <div className="fc">
+
+                            <div className="input-group">
+                                <input className="input--style-3" type="text" onChange={e => sethouseno(e.target.value)} placeholder="House no" name="email" required />
+
+                            </div>
+
+                            <div className="input-group">
+                                <input className="input--style-3" type="text" onChange={e => setstreetname(e.target.value)} placeholder="Street Name" name="email" required />
+
+                            </div>
 
                         </div>
-
-                           </div>
 
 
                         <div className="p-t-10 text-center">
@@ -237,10 +272,10 @@ export default function Signup() {
                         </div>
                     </form>
 
-        </div>
-    </div>
+                </div>
+            </div>
 
 
-    </>
-  )
+        </>
+    )
 }
